0b2f2143fcb2268285911b82b32c0708a3f7974c
Daniel Paul Wunderlich <wunderld@ecelnx44.ecn.purdue.edu>
2026-04-04 23:23:26 -0400

    OG map displaying to tft, need to fix tiles 7-10

--- include/display.h
+++ include/display.h
@@ -21,9 +21,11 @@
 #define NUM_TILES_X 28
 #define NUM_TILES_Y 31
 
+// Colors
 #define BLACK ((uint16_t)0x0000)
 #define BLUE ((uint16_t)0x001F)
 
+// Display Functions
 void tft_write_command(uint8_t cmd);
 void tft_write_data(uint8_t data);
 void tft_set_address_window(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
@@ -32,7 +34,54 @@ void display_init(void);
 void tft_write_tile(const uint16_t* tile, uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
 void draw_map(void);
 
+// Tile Sprites
 extern const uint16_t tile_1[TILE_HEIGHT * TILE_WIDTH];
 extern const uint16_t tile_2[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_3[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_4[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_5[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_6[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_7[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_8[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_9[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_10[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_11[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_12[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_13[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_14[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_15[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_16[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_17[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_18[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_19[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_20[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_21[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_22[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_23[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_24[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_25[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_26[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_27[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_28[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_29[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_30[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_31[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_32[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_33[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_34[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_35[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_36[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_37[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_38[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_39[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_40[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_41[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_42[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_43[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_44[TILE_HEIGHT * TILE_WIDTH];
+extern const uint16_t tile_45[TILE_HEIGHT * TILE_WIDTH];
+
+// Default map
+extern const uint8_t tile_map[NUM_TILES_Y][NUM_TILES_X];
 
 #endif
