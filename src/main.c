5d73a848b3aacaf12b4b5121b1e99fc173a235d2
Daniel Paul Wunderlich <wunderld@ecelnx42.ecn.purdue.edu>
2026-04-12 17:47:54 -0400

    pacman all directions good

--- src/main.c
+++ src/main.c
@@ -6,15 +6,36 @@
 #include "hardware/spi.h"
 #include "display.h"
 #include "controls.h"
+#include "queue.h"
 
+// For Keypad Testing
+uint16_t key_pop() {
+    // Queue is empty
+    while (kev.head == kev.tail) {
+        sleep_ms(10);   // Wait for an event to be pushed
+    }
+    uint16_t value = kev.q[kev.tail];
+    kev.tail = (kev.tail + 1) % 32;
+    return value;
+}
+
+void key_push(uint16_t value) {
+    // Queue is full, drop new keys
+    if ((kev.head + 1) % 32 == kev.tail) {
+        return; 
+    }
+    kev.q[kev.head] = value;
+    kev.head = (kev.head + 1) % 32;
+}
 
 // Controls Struct (for testing rn)
 InputState current_input = {
-    .joystick = INPUT_DIRECTION_LEFT,
+    .joystick = INPUT_DIRECTION_NONE,
     .start_pressed = true,
     .select_pressed = false
 };
 
+// Pacman struct, contains initial position
 PacmanState pacman = {
     .y = 23,
     .x = 13,
@@ -31,12 +52,36 @@ int main(){
     display_init();
     tft_fill_screen(BLACK);
     sleep_ms(100);
+
+    // Initialize keypad pins and timer for testing
+    keypad_init_pins();
+    keypad_init_timer();
     
     // Draw Map and Initial Pacman 
     draw_map();
     draw_pacman(current_input, pacman);
     
     for(;;){
+        // FOR TESTING WITH KEYPAD v =======
+
+        uint32_t keyevent = key_pop();
+            if (keyevent & (1 << 8)) {
+                char button = (char) (keyevent & 0xFF);
+                if(button == 'C'){
+                    current_input.joystick = INPUT_DIRECTION_UP;
+                }
+                else if(button == '#'){
+                    current_input.joystick = INPUT_DIRECTION_RIGHT;
+                }
+                else if(button == '8'){
+                    current_input.joystick = INPUT_DIRECTION_DOWN;
+                }
+                else if(button == '6'){
+                    current_input.joystick = INPUT_DIRECTION_LEFT;
+                }
+            }
+
+        // FOR TESTING WITH KEYPAD ^ =======
 
         // Update Pacman pos
         update_pacman(current_input, &pacman);
